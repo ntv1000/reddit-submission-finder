34a3eacbdc786c9bec2af3d8cf78f3e8b1dc21e1
ntv1000 <christian.ntv@hotmail.com>
2015-02-06 16:27:25 +0100

    add special treatment for youtube links
    this solves redirect issues for links to
    - m.youtube.com
    - youtu.be

--- data/panel.js
+++ data/panel.js
@@ -34,22 +34,39 @@ function getAllSubmissions(url){
 }
 
 function getAllURLVersions(url){
-	var without_http = "";
-	var result = ['http://www.reddit.com/api/info.json?url=' + encodeURIComponent(url)];
-	if (url.indexOf('https') === 0) {
-		without_http = url.substring(5);
-		result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent(without_http));
-		result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent("http" + without_http));
-	}
-	else{
-		if (url.indexOf('http') === 0) {
-			without_http = url.substring(4);
+	var host = url.split("/")[2];
+	var result = [];
+	if ((host === 'youtube.com' || host === 'www.youtube.com') && url.split("/")[3].indexOf('watch?') === 0) {
+		youtubeID = function () {
+			var query = url.substring(url.indexOf('?') + 1);
+			var parameters = query.split('&');
+			for (var i=0;i<parameters.length;i++) {
+				var pair = parameters[i].split('=');
+				if (pair[0] === 'v') {
+					return pair[1];
+				}
+			} 
+			return '';
+		} ();
+		result.push('http://api.reddit.com/search.json?q=' + encodeURIComponent('(url:' + youtubeID + ') (site:youtube.com OR site:youtu.be)'))
+	} else {
+		var without_http = "";
+		result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent(url));
+		if (url.indexOf('https') === 0) {
+			without_http = url.substring(5);
 			result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent(without_http));
-			result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent("https" + without_http));
+			result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent("http" + without_http));
 		}
 		else{
-			result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent("http" + without_http));
-			result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent("https" + without_http));
+			if (url.indexOf('http') === 0) {
+				without_http = url.substring(4);
+				result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent(without_http));
+				result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent("https" + without_http));
+			}
+			else{
+				result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent("http" + without_http));
+				result.push('http://www.reddit.com/api/info.json?url=' + encodeURIComponent("https" + without_http));
+			}
 		}
 	}
 	return result;
